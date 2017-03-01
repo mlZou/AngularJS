22c905e373a48f0ed327f6cc338f09a56e57342e
mlZou <2218219509@qq.com>
2017-03-01 19:33:57 +0800

    完成news的基本跳转获取数据功能

--- 0223/news/js/base.js
+++ 0223/news/js/base.js
@@ -1,27 +1,50 @@
-var app = angular.module('newsApp',['controllers','routers','directives']);
+var app = angular.module('newsApp',['ng.post','controllers','routers','directives','services']);
 
 var controllers = angular.module('controllers',[]);
-controllers.controller('indexCtrl',['$scope','$http',function($scope,$http){
+
+//主页控制器
+controllers.controller('indexCtrl',['$window','cookie','$scope','$http',function($window,cookie,$scope,$http){
+	$http.post('http://10.16.155.28:81/news/php/index.php/login_api/auto_login',{
+		params:{
+			username:cookie.getCookie('username'),
+			token:cookie.getCookie('token')
+		}
+	}).success(function(data){
+		if(data){
+			console.log(data);
+		}else{
+			$window.location.href = '#/login';
+		}
+	})
 	$scope.tabs = 1;
 	$scope.changeTab = function(num){
 		$scope.tabs = num;
 	}  
-}]).controller('recommendCtrl',['$scope','$http',function($scope,$http){
+}])
+//推荐控制器
+.controller('recommendCtrl',['$window','$scope','$http','$state',function($window,$scope,$http,$state){
+	//搜索
 	$scope.isSearch = false;
+	$scope.searchName = '';
 	$scope.search = function() {
 		$scope.isSearch = true;
 	}
 	$scope.cancel = function(){
 		$scope.isSearch = false;
 	}
-	$scope.searchName = '';
-	$scope.sort = true;
+	
+	//新闻排序方式
+	$scope.sort = false;
+	
+	//排序弹窗
+	$scope.isShowActionSheet = false;
 	$scope.showActionSheet = function(){
 		$scope.isShowActionSheet = true;
 	}
 	$scope.hideActionSheet = function(){
 		$scope.isShowActionSheet = false;
 	}
+	//点击更多加载数据
 	$scope.isLoadMore = true;
 	$scope.page = 0;
 	$scope.news =[];
@@ -39,7 +62,9 @@ controllers.controller('indexCtrl',['$scope','$http',function($scope,$http){
 		})
 	}
 	$scope.loadMore();
-}]).controller('detailsCtrl',['$scope','$http','$state',function($scope,$http,$state){
+}])
+//详情页控制器
+.controller('detailsCtrl',['$scope','$http','$state',function($scope,$http,$state){
 	console.log($state);
 	$http.get('http://10.16.155.28:81/news/php/index.php/news_api/show_detail',{
 		params:{
@@ -48,6 +73,69 @@ controllers.controller('indexCtrl',['$scope','$http',function($scope,$http){
 	}).success(function(data){
 		console.log(data);
 		$scope.news = data.news_list[0];
+	})
+}])
+//注册控制器
+.controller('registerCtrl',['$window','$scope','$http',function($window,$scope,$http){
+	$scope.goLogin = function(){
+		$http.post('http://10.16.155.28:81/news/php/index.php/login_api/register',{
+			params:{
+				username:$scope.username,
+				password:$scope.password
+			}
+		}).success(function(data){
+			if(data){
+				$window.location.href = '#/login';
+			}else{
+				$window.location.href = '#/register';
+			}
+			// console.log(data);
+		})
+	}
+	
+}])
+//登录控制器
+.controller('loginCtrl',['cookie','$window','$scope','$http',function(cookie,$window,$scope,$http){
+	console.log($scope.username);
+	console.log($scope.password)
+	$scope.goIndex = function(){
+		$http.post('http://10.16.155.28:81/news/php/index.php/login_api/login',{
+			params:{
+				username:$scope.username,
+				password:$scope.password
+			}
+		}).success(function(data){
+			console.log(data);
+			cookie.setCookie('username', data.user_name);
+			cookie.setCookie('token', data.info.token);
+			$window.location.href = '#/index/recommend';
+		})
+	}
+}])
+.controller('insertCtrl',['$window','$scope','$http',function($window,$scope,$http){
+	$scope.insertNews = function(){
+		$http.get('http://10.16.155.28:81/news/php/index.php/news_api/insert_news',{
+			params:{
+				title:$scope.title,
+				text:$scope.text
+			}
+		}).success(function(data){
+			console.log(data);
+		})
+	}
+}])
+//热点控制器
+.controller('hotCtrl',['$scope','$http',function($scope,$http){
+	$scope.isLoadMore = false; 
+	$http.get('http://10.16.155.28:81/news/php/index.php/news_api/show_detail_by_channel_id',{
+		params:{
+			page:1,
+			channel_id:7
+		}
+	}).success(function(data){
+		$scope.isLoadMore = true; 
+		console.log(data);
+		$scope.news = data.news_list;
 	})
 }])
 
@@ -66,6 +154,18 @@ routers.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$u
 		url:'/details/:id',
 		templateUrl:'template/details.html',
 		controller:'detailsCtrl'
+	}).state('register',{
+		url:'/register',
+		templateUrl:'template/register.html',
+		controller:'registerCtrl'
+	}).state('login',{
+		url:'/login',
+		templateUrl:'template/login.html',
+		controller:'loginCtrl'
+	}).state('insert',{
+		url:'/insert',
+		templateUrl:'template/insert.html',
+		controller:'insertCtrl'
 	});
 	//第二层(index下的html结构)
 	$stateProvider.state('index.recommend',{
@@ -74,7 +174,8 @@ routers.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$u
 		controller:'recommendCtrl'
 	}).state('index.hot',{
 		url:'/hot',
-		templateUrl:'template-sub/hot.html'
+		templateUrl:'template-sub/hot.html',
+		controller:'hotCtrl'
 	}).state('index.happy',{
 		url:'/happy',
 		templateUrl:'template-sub/happy.html'
@@ -120,4 +221,26 @@ directives.directive('nheader',function(){
 		templateUrl:'directive/nactionsheet.html'
 	}
 })
+
+//服务的模块
+var services = angular.module('services', []);
+//cookie服务
+services.service('cookie', ['$document', function($document) {
+	return {
+		setCookie: function(name, value) {
+			var days = 10;
+			var ex = new Date();
+			ex.setTime(ex.getTime() + days * 24 * 60 * 60 * 1000);
+			$document[0].cookie = name + "=" + value + ";expires=" + ex;
+		},
+		getCookie: function(name) {
+			var a;
+			var reg = new RegExp("(^|)" + name + "=([^;]*)(;|$)");
+			if(a = $document[0].cookie.match(reg)) {
+				return a[2];
+			}
+		}
+	}
+}])
+
 
